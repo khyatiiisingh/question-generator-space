a3a2e28a90ff77855c928833208aefa7d4dc8d80
khyatiiisingh <154037137+khyatiiisingh@users.noreply.github.com>
2025-05-15 06:49:14 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
+pip install google.generativeai
 from google.generativeai import configure, GenerativeModel
 import os
 
-os.environ["GOOGLE_API_KEY"] = "AIzaSyD3pO5KV4CJIIhRUqANsY-SG-SmOjW56zc"
+os.environ["GOOGLE_API_KEY"] = "AIzaSyBTg5V8q8BXKUx-3RQknorI9Wt6OXRJpIU"
 configure(api_key=os.environ["GOOGLE_API_KEY"])
 
 model = GenerativeModel("gemini-1.5-pro")
